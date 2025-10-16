7037359a9f55c81a4048f036201e8487cc801efc
unknown <mmdaggu2@gamil.com>
2025-10-16 11:07:38 +0900

    수정

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.js'
 import Viewstory from './Viewstory.jsx'
 import Profile from './Profile.jsx'
 
